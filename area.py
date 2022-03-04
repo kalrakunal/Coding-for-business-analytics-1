fd31cb44dbabb375232768a25a30b0cf13ba8de8
kalrakunal <100896891+kalrakunal@users.noreply.github.com>
2022-03-04 10:45:22 +0530

    Area.py
    Python program for calculating area of rectangle

---
+++ area.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#first we have to input the width and height of the rectangle
+l = float(input('Enter width: '))
+b = float(input('Enter height: '))
+#for the input, float is used for recognising the decimal values
+#now, both inputted values will be used for calculating area of reactangle
+Area = l * b
+#now, after calculating area, it is been printed
+print("Area: %.2f" %Area)
+
+
+# In[ ]:
+
+
+
+
