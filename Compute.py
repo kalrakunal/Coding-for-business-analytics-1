4c128f7fedf9652711cf6d1a4d67f3d64d122dc6
kalrakunal <100896891+kalrakunal@users.noreply.github.com>
2022-03-25 13:39:22 +0530

    Compute.py

---
+++ Compute.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[90]:
+
+
+class shapes:
+    def __init__ (self, l):
+        self.l = []
+        
+        
+    def dicts():
+        a = []
+        d1 = {"type": "Square", "area": 150.5}
+        d2 = {"type": "Rectangle", "area": 80}
+        d3 = {"type": "Rectangle", "area": 660}
+        d4 = {"type": "Cicle", "area": 68.2}
+        d5 = {"type": "Triangle", "area": 20}
+        a.append(d1)
+        a.append(d2)
+        a.append(d3)
+        a.append(d4)
+        a.append(d5)
+        return a
+    
+z = shapes.dicts()
+
+print("1 -", (z[0]["type"]), "with area size" ,(z[0]["area"]))
+print("1 -", (z[1]["type"]), "with area size" ,(z[1]["area"]))
+print("1 -", (z[2]["type"]), "with area size" ,(z[2]["area"]))
+print("1 -", (z[3]["type"]), "with area size" ,(z[3]["area"]))
+print("1 -", (z[4]["type"]), "with area size" ,(z[4]["area"]))
+
+
+# In[ ]:
+
+
+
+
